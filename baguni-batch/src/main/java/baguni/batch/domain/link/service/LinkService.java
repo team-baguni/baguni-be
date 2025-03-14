418b947c2633e33d185767af547ed9fa71bd0576
kimminkyeu <minkyeki@student.42seoul.kr>
2025-03-14 16:57:01 +0900

    refactor: 리팩토링한 내용을 linkService에 종합 반영

--- baguni-batch/src/main/java/baguni/batch/domain/link/service/LinkService.java
+++ baguni-batch/src/main/java/baguni/batch/domain/link/service/LinkService.java
@@ -1,11 +1,13 @@
 package baguni.batch.domain.link.service;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import baguni.batch.domain.feed.util.Llama32KoreanAgent;
-import baguni.batch.domain.link.dto.LinkAnalyzeResult;
+import baguni.batch.domain.analyzer.ArticleAnalyzer;
+import baguni.batch.domain.link.dto.LinkCrawlResult;
 import baguni.infra.model.link.Link;
 import baguni.infra.infrastructure.link.LinkDataHandler;
 import baguni.infra.infrastructure.link.dto.LinkMapper;
@@ -21,10 +23,16 @@ import lombok.extern.slf4j.Slf4j;
 public class LinkService {
 
 	private final LinkDataHandler linkDataHandler;
-	private final LinkAnalyzer linkAnalyzer;
+	private final LinkCrawler linkCrawler;
 	private final LinkMapper linkMapper;
 
-	private final Llama32KoreanAgent llamaAgent;
+	private ArticleAnalyzer articleAnalyzer;
+
+	@Autowired
+	@Qualifier("local-ollama3.2-korean")
+	public void setArticleAnalyzer(ArticleAnalyzer articleAnalyzer) {
+		this.articleAnalyzer = articleAnalyzer;
+	}
 
 	@WithSpan
 	@Transactional(readOnly = true)
@@ -37,18 +45,16 @@ public class LinkService {
 	@Transactional
 	public void analyzeAndUpdateLink(@SpanAttribute("url") String url) {
 		Link link = linkDataHandler.getLink(url);
-		LinkAnalyzeResult result = linkAnalyzer.analyze(url);
+		LinkCrawlResult crawlResult = linkCrawler.crawl(url);
 
-		if (StringUtils.isNotEmpty(result.title())) {
-			link.updateMetadata(result.title(), result.description(), result.imageUrl());
+		if (StringUtils.isNotEmpty(crawlResult.title())) {
+			link.updateMetadata(crawlResult.title(), crawlResult.description(), crawlResult.imageUrl());
 		}
 
-		// 본문 크롤링 데이터 꺼내서 처리하기
-		// TODO: 1. youtube 링크는 어떻게 작동할지 확실하지 않아서, 일단 Feed 블로그만 하도록 처리
-		//       2. 링크 테이블 isRss를 isFeed로 변경 필요
-		String content = result.content();
+		// TODO: youtube 링크는 어떻게 작동할지 확실하지 않아서, 일단 Feed 블로그만 하도록 처리
 		if (link.isBlogFeed()) {
-			String summary = llamaAgent.summarize(content).response(); // 요약 결과
+			String summary = articleAnalyzer.summarize(crawlResult.content());
+			log.info("요약 결과 = {}", summary);
 			link.updateSummary(summary);
 		}
 	}
