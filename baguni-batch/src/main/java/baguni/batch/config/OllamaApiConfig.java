e360ba0ff64a7a46eba86f7386143696e3503652
Minky <minkyeki@student.42seoul.kr>
2025-03-16 17:23:09 +0900

    fix: 스케쥴 딜레이 변경, 에러 try catch 추가 (#62)

--- baguni-batch/src/main/java/baguni/batch/config/OllamaApiConfig.java
+++ baguni-batch/src/main/java/baguni/batch/config/OllamaApiConfig.java
@@ -17,8 +17,8 @@ import baguni.batch.domain.analyzer.ollama.OllamaApi;
 @Configuration
 public class OllamaApiConfig {
 
-	private static final Duration CONNECTION_TIMEOUT = Duration.ofSeconds(60 * 5);
-	private static final Duration READ_TIMEOUT = Duration.ofSeconds(60 * 5);
+	private static final Duration CONNECTION_TIMEOUT = Duration.ofMinutes(1);
+	private static final Duration READ_TIMEOUT = Duration.ofMinutes(1);
 	private static final String OLLAMA_SERVER_URL = "http://ollama:11434";
 	private static final String OLLAMA_LOCAL_URL = "http://localhost:11434"; // local에서 docker로 띄운 llama 사용 시 필요
 
