0f653706677b58bbe37e439ecedbfbf8ec271b41
Haedeok Song <86779839+ss0ngcode@users.noreply.github.com>
2025-03-18 15:27:15 +0900

    fix: Ollama Timeout 확인용 log 추가 (#74)

--- baguni-batch/src/main/java/baguni/batch/domain/ai/ollama/OllamaAgent.java
+++ baguni-batch/src/main/java/baguni/batch/domain/ai/ollama/OllamaAgent.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Component;
 
 import baguni.batch.domain.ai.AiAgent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component("llama3.2-korean")
 @RequiredArgsConstructor
 public class OllamaAgent implements AiAgent {
@@ -14,6 +16,8 @@ public class OllamaAgent implements AiAgent {
 
 	@Override
 	public String ask(String prompt) {
+		log.info("length: {}, prompt: {}", prompt.length(), prompt);
+
 		return ollamaApi
 			.sendRequest(OllamaRequest
 				.builder()
