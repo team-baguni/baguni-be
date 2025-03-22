b11ae86d150a1834c27798d1f0cfc4843d53619c
kimminkyeu <minkyeki@student.42seoul.kr>
2025-03-22 23:46:43 +0900

    feat: ddl 스크립트 작성

---
+++ baguni-infra/src/main/resources/db/migration/V6.0__init_article_tables.sql
@@ -0,0 +1,86 @@
+create table `link_v2`
+(
+    id            bigint auto_increment
+        primary key,
+    created_at    datetime(6)      not null,
+    updated_at    datetime(6)      not null,
+    guid          binary(16)       not null,
+    is_accessible bit default b'1' not null,
+    url           varchar(2048)    not null,
+    constraint UKhsobr1elx8v8phbqymq08sifd
+        unique (guid)
+);
+
+create table article_detail
+(
+    id         bigint auto_increment
+        primary key,
+    created_at datetime(6) not null,
+    updated_at datetime(6) not null,
+    content    text        null,
+    summary    text        null
+);
+
+create table article
+(
+    id         bigint auto_increment
+        primary key,
+    created_at datetime(6) not null,
+    updated_at datetime(6) not null,
+    image_url  text        null,
+    title      text        null,
+    detail_id  bigint      null,
+    link_id    bigint      null,
+    constraint UKa1o1pkpy09ngnhyur344dq8kp
+        unique (detail_id),
+    constraint UKhcr91j53wrjmec2hwphfpgokx
+        unique (link_id),
+    constraint FK8gsrxlo8iat28wlr8d3q5f0ct
+        foreign key (link_id) references `link_v2` (id),
+    constraint FKm3qh63x5ta2gvgf6kafdl3084
+        foreign key (detail_id) references article_detail (id)
+);
+
+create table category
+(
+    id         bigint auto_increment
+        primary key,
+    created_at datetime(6)  not null,
+    updated_at datetime(6)  not null,
+    name       varchar(255) not null,
+    constraint UK46ccwnsi9409t36lurvtyljak
+        unique (name)
+);
+
+create table keyword
+(
+    id         bigint auto_increment
+        primary key,
+    created_at datetime(6)  not null,
+    updated_at datetime(6)  not null,
+    name       varchar(255) not null,
+    constraint UKhvq9bm3mbguqoicyv02g5crjs
+        unique (name)
+);
+
+create table article_category
+(
+    catetory_id bigint null,
+    article_id  bigint not null
+        primary key,
+    constraint FKd61g098hytbe0i2irhjmffk4n
+        foreign key (catetory_id) references category (id),
+    constraint FKrw5912jiy0vyqoyqlo5r65igk
+        foreign key (article_id) references article (id)
+);
+
+create table article_keyword
+(
+    article_id bigint not null,
+    keyword_id bigint not null,
+    primary key (article_id, keyword_id),
+    constraint FK71143jtrpbywwr8ys2eppe7c2
+        foreign key (article_id) references article (id),
+    constraint FKkuyvit2pwelitlj3aulwqrsb5
+        foreign key (keyword_id) references keyword (id)
+);
