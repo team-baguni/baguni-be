a1617e8cbc1d34ee85be0b4f3373d969363ee855
Minkyeu Kim <minkyeki@student.42seoul.kr>
2025-03-23 15:05:21 +0900

    [FIX] 프롬프트 결과에서 \\* 강조 제거 (#93)
    * fix: 프롬프트 결과에서 \\* 강조 제거
    
    * [FEAT] 카테고리 선정 방식 개선 (#94)
    
    * feat: 카테고리/서브카테고리 병합
    
    * refactor: 카테고리 선언 방식 정의
    
    * refactor: 메서드명 변경
    
    * refactor: enum 메서드명 수정

--- baguni-batch/src/main/java/baguni/batch/domain/ai/ollama/OllamaAgent.java
+++ baguni-batch/src/main/java/baguni/batch/domain/ai/ollama/OllamaAgent.java
@@ -45,32 +45,29 @@ public class OllamaAgent extends AiAgent {
 		return tryCatchWrapper("Ollama 카테고리 추출", () -> {
 			var request = OllamaChatRequestBuilder.Categorize(MODEL, text);
 			var response = ollamaApi.chat(request);
-			var resultJsonString = response.message().content();
-			var result = new ObjectMapper().readValue(resultJsonString, OllamaCategorizeResult.class);
-			return result.category();
+			var category = new ObjectMapper()
+				.readValue(response.message().content(), OllamaCategorizeResult.class)
+				.category();
+			return cleanWords(category);
 		});
 	}
 
 	@Override
 	public List<String> getKeywords(String text) {
 		return tryCatchWrapper("Ollama 키워드 목록 추출", () -> {
-			var keywords = new ArrayList<String>();
-
-			// TODO: (개발 --> 개발 하위 카테고리 선택) 처럼 하위 카테고리 별로 다른 프롬프트 실행할 것
-			var request1 = OllamaChatRequestBuilder.SubCategorize(MODEL, text);
-			var response1 = ollamaApi.chat(request1);
-			var result1JsonString = response1.message().content();
-			var result1 = new ObjectMapper().readValue(result1JsonString, OllamaSubCategorizeResult.class);
-			keywords.add(result1.subcategory());
-
-			var request2 = OllamaChatRequestBuilder.GetKeywords(MODEL, text);
-			var response2 = ollamaApi.chat(request2);
-			var result2JsonString = response2.message().content();
-			var result2 = new ObjectMapper().readValue(result2JsonString, OllamaGetKeywordsResult.class);
-			keywords.addAll(result2.keywords());
-
-			return keywords;
+			var request = OllamaChatRequestBuilder.GetKeywords(MODEL, text);
+			var response = ollamaApi.chat(request);
+			var keywords = new ObjectMapper()
+				.readValue(response.message().content(), OllamaGetKeywordsResult.class)
+				.keywords();
+			return keywords.stream().map(this::cleanWords).toList();
 		});
+	}
+
+	// 강조 표시 특수 문자 등의 프롬프트 꾸미기 문자 제거
+	// 간혹 프롬프트 결과에서 **Backend** 처럼 강조가 붙는 경우 있음.
+	private String cleanWords(String word) {
+		return word.replaceAll("\\*", "");
 	}
 
 	private <T> T tryCatchWrapper(String action, ThrowingSupplier<T> supplier) {
